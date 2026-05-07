5eb367a6c25c477719aee3d020eb735ab1198ede
yoavbuzali21 <yoavb2105@gmail.com>
2026-05-07 10:42:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,19 @@ def generate_dataset(n=2000, noise_ratio=0.05, random_state=42):
     df["latitude"] = df["latitude"].round(4)
     df["longitude"] = df["longitude"].round(4)
 
+
+
+    
+
     return df
 
 df = generate_dataset()
 st.dataframe(df)
+
+
+st.scatter_chart(
+    df,
+    x="longitude",
+    y="latitude"
+    color="launch_to_israel"
+)
